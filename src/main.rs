6feee1c452afb27663d0e27ff38bc3bc81d85152
pna70 <pna70@icloud.com>
2023-03-17 09:17:01 +0000

    added gcd example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
+
+use std::str::FromStr;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut numbers:Vec<u64> = Vec::new();
+
+    for arg in env::args().skip(1) {
+
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+    
+    if numbers.len() == 0 {
+
+        eprintln!("Usage gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
 
 #[test]
